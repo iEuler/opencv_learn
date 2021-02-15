5bc2889b4a1948e096722b4c1e3084667d5e8d17
iEuler <48609238+iEuler@users.noreply.github.com>
2021-02-15 10:08:08 -0500

    add tests for opencv

---
+++ opencv01.py
@@ -0,0 +1,17 @@
+import cv2
+
+filepath = r"img/pic01.jpg"
+
+# img = cv2.imread(filepath)
+# cv2.namedWindow('Image')
+# cv2.imshow('Image', img)
+# cv2.waitKey(1000)
+# cv2.destroyAllWindows()
+#
+img = cv2.imread(filepath)
+# 转换灰色
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# 显示图像
+cv2.imshow("Image", gray)
+cv2.waitKey(1000)
+cv2.destroyAllWindows()
